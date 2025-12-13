6b4128d121ea46f19203e75747f1947342123fa2
novikevichpavel <novikevichp@gmail.com>
2025-12-13 10:53:41 +0300

    Added java

---
+++ java/Learning/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        String c ;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first num: ");
+        a = scanner.nextInt();
+
+        System.out.println("Enter second num");
+        b = scanner.nextInt();
+
+        scanner.nextLine();
+
+
+
+
+    }
+}
