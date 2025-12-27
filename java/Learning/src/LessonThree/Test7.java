e7881228408e74d43fd6807081cb50f7c06d5b65
novikevichpavel <novikevichp@gmail.com>
2025-12-27 09:50:08 +0300

    Nothing interesting

---
+++ java/Learning/src/LessonThree/Test7.java
@@ -0,0 +1,18 @@
+package LessonThree;
+
+public class Test7 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int c = 0;
+        int d = 100;
+
+
+        if(a > b && c < d){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
+}
---
+++ java/Learning/src/LessonThree/Test8.java
@@ -0,0 +1,10 @@
+package LessonThree;
+
+public class Test8 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a | b);
+    }
+}
